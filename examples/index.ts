db3f3d6cff1b1db38cb18734f34769edd27ff893
ramin <raminjafary1993@gmail.com>
2020-11-13 23:43:34 +0330

    refactor: remove scheduler

--- examples/index.ts
+++ examples/index.ts
@@ -1,15 +1,14 @@
 import { MemoryCache, storageCache } from '../src'
-import { AsyncCache } from '../src/scheduler'
 
 const cache = MemoryCache.of(2)
 
 fetch('https://jsonplaceholder.typicode.com/posts?userId=1', {
   headers: {
-    'Access-Control-Allow-Origin': '*'
-  }
+    'Access-Control-Allow-Origin': '*',
+  },
 })
-  .then(json => json.json())
-  .then(data => {
+  .then((json) => json.json())
+  .then((data) => {
     storageCache.sessionStorage.setItem('data', data)
     cache.set('data2', data)
     cache.track(
@@ -19,36 +18,6 @@ fetch('https://jsonplaceholder.typicode.com/posts?userId=1', {
       }),
       true
     )
+    storageCache.sessionStorage.setItem('todoOne', data)
+    cache.set('todoTwo', data)
   })
-
-const userPosts = new AsyncCache(async function userPosts() {
-  const res = await fetch(
-    'https://jsonplaceholder.typicode.com/posts?userId=1',
-    {
-      headers: {
-        'Access-Control-Allow-Origin': '*'
-      }
-    }
-  )
-  const data = res.json()
-  return data
-})
-;(async () => {
-  const data = await userPosts.proxy()
-  console.log('---DATA COMES FROM ASYNC/AWAIT---\n', data)
-  storageCache.sessionStorage.setItem('todoOne', data)
-})()
-
-const postTwo = new AsyncCache((id: number) => {
-  return fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
-    headers: {
-      'Access-Control-Allow-Origin': '*'
-    }
-  })
-    .then((res: any) => res.json())
-    .then((json: any) => json)
-})
-postTwo.proxy(2).then(data => {
-  console.log('---DATA COMES FROM PROMISE---\n', data)
-  cache.set('todoTwo', data)
-}, console.error)
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,2 @@
 export { MemoryCache } from '../src/memory-cache'
 export { storageCache } from '../src/storage'
-export { AsyncCache } from '../src/scheduler'
